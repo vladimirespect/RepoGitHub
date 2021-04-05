2955777c71b5b74a95a7831aa18d6647da29ddab
Vladimir <vladimirespect@mail.ru>
2021-04-05 14:19:51 +0300

    refresh

--- PHP 2 HW 5/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//конфигурация подключения, пока использована только в автозагрузчике
-define('ROOT_DIR', dirname(__DIR__));//это нарушение ООП поправим к 8му уроку
-define('DS', DIRECTORY_SEPARATOR);
-define('CONTROLLER_NAMESPACE', 'app\\controllers\\');
-define('VIEWS_DIR', '../views/');
-
-//TODO можно попробовать самостоятельно организовать хранение констант в классе через статику
